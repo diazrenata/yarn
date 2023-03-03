e351ec87568ace70667e4fd2547e0cab34f72d1b
diazrenata <bbssizeshifts@gmail.com>
2023-03-03 11:19:05 -0500

    compiles

---
+++ R/toy.R
@@ -0,0 +1,15 @@
+#' Toy
+#'
+#' @param a_word who likes yarn
+#'
+#' @return a sentence
+#' @export
+#'
+#' @examples
+#' 
+#' toy("possums")
+#' 
+toy <- function(a_word) {
+    
+    paste0(a_word, ", like cats, likes yarn.")
+}
